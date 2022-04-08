59306aad009899ac97da42583093433a53d5d14d
Parshad Cholera <pcholera@hartreepartners.com>
2022-04-08 18:10:20 +0530

    Initial Commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RunnerTest;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new ("Parshad", 50);
+            account.Credit(30);
+            account.Debit(10);
+
+            Console.WriteLine("Available Balance = " + account.Balance);
+            Console.ReadLine();
+        }
+    }
+}
